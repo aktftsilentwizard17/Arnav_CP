66430722f9f8bb203dff641548173ef58e3f8e44
arnavk <akatepallewar@gmail.com>
2026-09-11 01:40:15 +0530

    Add Halloumi Boxes [A] from Codeforces

---
+++ 1903/A - Halloumi Boxes/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n,k;
+        cin>>n>>k;
+        vector<long long> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        vector<long long> copyofa = a;
+        sort(copyofa.begin(),copyofa.end());
+        if(copyofa==a||k>1){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
